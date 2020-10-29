a91fb6a287b7ab7193fc3fcd074178dfafec4780
Bernard Fok <bernard_fok@outlook.com>
2020-10-29 16:11:51 -0700

    Initial commit

---
+++ component---src-pages-404-js-298d09c4289fefcf34a0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"4z4a":function(e,t,a){},w2l6:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),c=a.n(n),l=a("Wbzz"),o=a("Bl7J"),r=a("vrFN");a("4z4a");t.default=function(){return c.a.createElement(o.a,null,c.a.createElement(r.a,{title:"404: Not found"}),c.a.createElement("div",{className:"container"},c.a.createElement("h1",{className:"hugeTitle"},"404 Page Not Found"),c.a.createElement(l.Link,{to:"/",className:"rectBtn--blue returnBtn"},"Go Home")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-298d09c4289fefcf34a0.js.map
